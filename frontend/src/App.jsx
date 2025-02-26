480adf73fd993eb77b05cb50880712ec0f8af156
Kaju2002 <IT22224002@my.sliit.lk>
2025-02-26 10:12:40 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './Pages/HomePage'
 
 const App = () => {
   return (
     <div>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   )
 }
